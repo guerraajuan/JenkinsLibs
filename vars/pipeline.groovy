28dbb3d1ff449ef82000c175d1651079022c7b7a
guerraajuan <guerraelec@gmail.com>
2023-04-17 17:49:03 -0400

    Se carga pipeline desde shared libraries

---
+++ vars/pipeline.groovy
@@ -0,0 +1,20 @@
+def call(Map params ){
+    pipeline {
+        agent any
+
+        stages {
+            stage('Paso 1') {
+                steps {
+                    echo "Hola: ${params.nombre}"
+                    
+                }
+            }
+            stage('Paso 2') {
+                steps {
+                    echo "Tu apellido es:  ${params.apellido}"
+                    
+                }
+            }
+        }
+    }
+}
