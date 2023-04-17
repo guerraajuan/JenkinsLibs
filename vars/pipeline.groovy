6962b5aff432246e030a0c245ed1c7d8e2383916
guerraajuan <guerraelec@gmail.com>
2023-04-17 18:08:15 -0400

    ajuste en variables

--- vars/pipeline.groovy
+++ vars/pipeline.groovy
@@ -1,17 +1,17 @@
-def call(Map params ){
+def call(String nombre ){
     pipeline {
         agent any
 
         stages {
             stage('Paso 1') {
                 steps {
-                    echo "Hola:"+ params.nombre
+                    echo "Hola: ${nombre}" 
                     
                 }
             }
             stage('Paso 2') {
                 steps {
-                    echo "Tu apellido es:" + params.apellido
+                    echo "Tu apellido es: ${nombre}"
                     
                 }
             }
